8e683fc5e1f554bdb988128e87636d7e929e690d
Stefan <floydrise@gmail.com>
2025-03-29 18:24:02 +0200

    Add CRUD operations to expenses endpoint & initialise frontend proj

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
-import {Hono} from 'hono'
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import expensesRoute from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono()
+  .use("*", logger())
+  .use("/favicon.ico", serveStatic({ path: "./favicon.ico" }))
+  .route("/api/expenses", expensesRoute);
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
