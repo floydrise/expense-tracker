441fea34a0acaf84ae6d7020b9344498ec5ad89e
Stefan <floydrise@gmail.com>
2025-03-30 22:32:00 +0300

    Add form to create-expense page

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,9 +1,29 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button.tsx";
 
 export const Route = createFileRoute("/create-expense")({
   component: CreateExpense,
 });
 
 function CreateExpense() {
-  return <div>Hello "/create-expense"!</div>;
+  return (
+    <div>
+      <h1>Create expense:</h1>
+      <form className={"max-w-xl m-auto"}>
+        <Label htmlFor="title" className={"text-lg mb-2"}>
+          Title
+        </Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount" className={"text-lg mb-2"}>
+          Amount
+        </Label>
+        <Input type="number" id="amount" placeholder="Amount" />
+        <Button className={"mt-4"} type={"submit"}>
+          Create expense
+        </Button>
+      </form>
+    </div>
+  );
 }
