6a042aa3c136c47ea18259fb3d366162deeed3df
Stefan <floydrise@gmail.com>
2025-04-03 23:41:47 +0300

    Add animations using motion

--- frontend/src/routes/_authenticated/index.tsx
+++ frontend/src/routes/_authenticated/index.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/lib/api.ts";
+import { motion } from "motion/react";
+import { Skeleton } from "@/components/ui/skeleton.tsx";
 
 export const Route = createFileRoute("/_authenticated/")({
   component: Index,
@@ -29,12 +31,24 @@ function Index() {
 
   if (error) return "An error occured" + error.message;
   return (
-    <Card className={"w-[350px] m-auto mt-10"}>
-      <CardHeader>
-        <CardTitle>Total spent</CardTitle>
-        <CardDescription>Total amount spent</CardDescription>
-      </CardHeader>
-      <CardContent>{isPending ? "Loading ..." : data.total}</CardContent>
-    </Card>
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+      className={"w-[320px] md:w-[350px] m-auto mt-10"}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>Total spent</CardTitle>
+          <CardDescription>Total amount spent</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {isPending ? <Skeleton className="h-4 w-12" /> : data.total}
+        </CardContent>
+      </Card>
+    </motion.div>
   );
 }
--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,19 +1,50 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { motion } from "motion/react";
 
 export const Route = createFileRoute("/about")({
   component: About,
 });
 
+const listVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.3 },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { type: "spring", bounce: 0.5 },
+  },
+};
+
 function About() {
+  const listItems = [
+    "Create account",
+    "Add new expenses",
+    "Track expenses",
+    "Delete expenses",
+  ];
+
   return (
-    <div className="flex flex-col justify-center items-center gap-2 mt-10">
-      <h1 className={"text-xl"}>Simple expense tracker app</h1>
-      <ul className={"list-disc"}>
-        <li>Create account</li>
-        <li>Add new expenses</li>
-        <li>Track expenses</li>
-        <li>Delete expenses</li>
-      </ul>
+    <div className="flex flex-col justify-center  items-center mt-10">
+      <h1 className={"text-xl border-b"}>Simple expense tracker app</h1>
+      <motion.ul
+        className={"list-disc"}
+        initial="hidden"
+        animate="visible"
+        variants={listVariants}
+      >
+        {listItems.map((item, index) => (
+          <motion.li key={index} variants={itemVariants}>
+            {item}
+          </motion.li>
+        ))}
+      </motion.ul>
     </div>
   );
 }
